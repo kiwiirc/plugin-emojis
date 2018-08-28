798ea25bc70d691c8522eb471000e62bf03fe16d
Scott McGann <smcgann@jsbot.net>
2018-08-28 18:37:09 -0400

    fixed dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
       exclude: /node_modules/,
       loader: 'babel-loader',
       query: {
-        presets: ['es2015'],
+        presets: ['@babel/preset-env'],
       }
     }]
   },
